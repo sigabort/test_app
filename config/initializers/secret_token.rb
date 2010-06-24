bc909d574c17ee0ee19b7a3d89435b1a9f16b708
sigabort <abort.coredump@gmail.com>
2010-06-24 11:09:10 -0400

    testing heroku bundler

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = 'c45aaa118e54ae007c35b40cde7fbeb34c1f260bb6937c53941dc4c89d91402121a3c8faea371eec6567f4da6d72d878043c14e216a6f8a303fa8f4b78dd3329'
